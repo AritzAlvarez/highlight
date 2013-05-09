6af5c50e0c846af98a9902d35011d40915a77ed3
Aritz Alvarez <aritzalvarez@gmail.com>
2013-05-10 01:05:41 +0300

    Update highlight.js

--- highlight.js
+++ highlight.js
@@ -1,11 +1,10 @@
-
 //main function
 
 $(function() { $( "#button_highlight" ).click(function() {
     
     //Toogles highlight_on classes to any elemente with .to_higlight class
-  $( ".to_highlight" ).toggleClass( "highlight_on", 1000 );
+  $( ".to_highlight" ).toggleClass( "highlight_on", 500 );
     
     //Toogles highlight_on classes to trigger button too
-    $( "#button_highlight" ).toggleClass( "highlight_on", 1000 );
+    $( "#button_highlight" ).toggleClass( "highlight_on", 500 );
 	return false; }); });
